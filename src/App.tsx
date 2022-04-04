b447dcf6974c6bd3914c742d7ddedc13cd2c4169
cw-jay <cw-jay@coway.com>
2022-04-04 16:03:00 +0900

    feat(CWCLIT-258): [개발] Molecule component 개발 및 login layout 구성
    - email list filter 및 관련 handler를 hook으로 따로 변경
    - email list의 like match를 regex 조건으로 검색하여 효율성 향상

--- src/App.tsx
+++ src/App.tsx
@@ -7,54 +7,15 @@ import Button from './atoms/Button'
 import Checkbox from './atoms/Checkbox'
 import Password from './molecules/Password'
 import Autocomplete from './molecules/Autocomplete'
-
-function filteredListForPartialMatch(arr: string[], sub: string): string[] {
-	sub = sub.toLowerCase()
-	return arr.filter(
-		(str) =>
-			sub.length <= str.length &&
-			str.toLowerCase().startsWith(sub.slice(0, str.length))
-	)
-}
+import useEmailFilter from './hooks/useEmailRecommend'
 
 function App() {
-	const [email, setEmail] = useState('')
-	const [isShowEmailOptions, setShowEmailOptions] = useState(false)
-
-	const emailList = ['gmail.com', 'naver.com', 'hanmail.net']
-	const emailRegex =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@/
-
-	const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(e.target.value)
-		if (emailRegex.test(e.target.value)) {
-			showEmailList(true)
-		} else {
-			showEmailList(false)
-		}
-	}
-
-	const showEmailList = (isShow: boolean) => {
-		setShowEmailOptions(isShow)
-	}
+	const emailFilterProps = useEmailFilter()
 	return (
 		<>
 			<div className="container">
 				<div className="row">
-					<Autocomplete
-						value={email}
-						onChange={onChangeEmail}
-						isShowOptions={isShowEmailOptions}
-						options={filteredListForPartialMatch(
-							emailList,
-							email.substring(email.lastIndexOf('@') + 1)
-						)}
-						prefixTextForOptions={email.substring(
-							0,
-							email.lastIndexOf('@') + 1
-						)}
-						placeholder="아이디 (이메일)"
-					/>
+					<Autocomplete {...emailFilterProps} placeholder="아이디 (이메일)" />
 				</div>
 				<div className="row">
 					<Checkbox id="autoLoginCheck" label="자동 로그인" />
---
+++ src/hooks/useEmailRecommend.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+export default function useEmailFilter() {
+	const [email, setEmail] = useState('')
+	const [isShowEmailOptions, setShowEmailOptions] = useState(false)
+	const emailList = ['gmail.com', 'naver.com', 'hanmail.net']
+
+	const emailRegex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@/
+
+	const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const isEmail = emailRegex.test(e.target.value)
+		setEmail(e.target.value)
+		showEmailList(isEmail)
+	}
+
+	const showEmailList = (isShow: boolean) => {
+		setShowEmailOptions(isShow)
+	}
+
+	const filteredListForEmailLikeMatch = (
+		arr: string[],
+		sub: string
+	): string[] => {
+		const reg = new RegExp('^' + sub.substring(sub.lastIndexOf('@') + 1), 'i')
+		return arr.filter((element) => element.match(reg))
+	}
+
+	const prefixTextForOptions = email.substring(0, email.lastIndexOf('@') + 1)
+
+	return {
+		...{
+			value: email,
+			isShowOptions: isShowEmailOptions,
+			prefixTextForOptions,
+			onChange: onChangeEmail,
+			options: filteredListForEmailLikeMatch(emailList, email)
+		}
+	}
+}
